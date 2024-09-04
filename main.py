9d042b14b1c29cae41c25fda3f05d48b75448eea
jonathan <jonathan.lemari@gmail.com>
2024-09-04 02:12:50 +0200

    first version of Qrcode generator. working locally.

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template, request
-# import qrcode 
+import qrcode 
+
+from io import BytesIO
+from base64 import b64encode
+
+import qr_code
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
     return render_template("home.html")
+
+
+@app.route("/", methods=["POST"])
+def generate_qr():
+    memory = BytesIO()
+    data = request.form.get("link")
+
+    img = qrcode.make(data)
+
+    img.save(memory)
+    memory.seek(0)
+
+    base64_img = "data:image/png;base64," + b64encode(memory.getvalue()).decode("ascii")
+
+    return render_template("home.html" ,data=base64_img)
 
 if __name__ == "__main__":
     app.run(debug=True)
