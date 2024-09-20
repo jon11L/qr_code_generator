b904372d3de81f18a1971c069bf099f8ff3f9063
jonathan <jonathan.lemari@gmail.com>
2024-09-20 04:26:45 +0200

    added download functionality, working on design.

--- README.md
+++ README.md
@@ -4,3 +4,22 @@
 ### the idea is to make a simple webpage for user to be able to generate new QR code.
 
 ### Learning about Flask framework in doing so.
+
+# QR Code Generator
+
+The purpose of this project is to create a simple web application where users can generate customized QR codes. The application is built using Python's Flask framework for the backend, and HTML/CSS for the frontend.
+
+## Features
+- Users can input a link or text and the app generates a QR code.
+- Customizable colors for the filling and background colors.
+- Download option on the user side, only temporarily saved on servers side.
+
+## Requirements
+Before running the app, ensure that the following dependencies are installed. They can be installed via the `requirements.txt` file.
+
+```bash
+pip install -r requirements.txt
+```
+
+
+
--- main.py
+++ main.py
@@ -4,13 +4,11 @@ import qrcode
 from io import BytesIO
 from base64 import b64encode
 
-# import qr_code
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 @app.route("/", methods=["POST"])
@@ -20,25 +18,29 @@ def generate_qr():
     # --- getting the datas from the user, 
     color_1 = request.form.get("fill-color")
     color_2 = request.form.get("background-color")
-    data = request.form.get("link")
+    data_passed = request.form.get("link")
 
-    # --- QRcode creation -----
-    qr = qrcode.QRCode(version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=15,
-    border=2,
-    )
+    if not data_passed:
+        # --- if no data is passed, return with a message
+        return render_template("index.html")
+    else:
+        # --- QRcode creation -----
+        qr = qrcode.QRCode(version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=15,
+        border=2,
+        )
 
-    qr.add_data(data)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color=color_1, back_color=color_2)
+        qr.add_data(data_passed)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color=color_1, back_color=color_2)
 
-    # ---- save the created image ---- 
-    img.save(memory)
-    memory.seek(0)
-    base64_img = "data:image/png;base64," + b64encode(memory.getvalue()).decode("ascii")
+        # ---- save the created image ---- 
+        img.save(memory)
+        memory.seek(0)
+        base64_img = "data:image/png;base64," + b64encode(memory.getvalue()).decode("ascii")
 
-    return render_template("home.html" ,data=base64_img)
+        return render_template("index.html" ,data=base64_img)
 
 
 if __name__ == "__main__":
